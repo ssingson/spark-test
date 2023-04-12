d934bc274e6fc9e2a160a33cf8b5cff02be534d3
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-04-12 13:14:12 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+ssh root@10.128.0.17 '/usr/local/hadoop/bin/hdfs --daemon start datanode'
+ssh root@10.128.0.18 '/usr/local/hadoop/bin/hdfs --daemon start datanode'
+
 /usr/local/hadoop/sbin/start-dfs.sh
 /usr/local/spark/sbin/start-all.sh
